2ac1582f29afcdc7302828040d79b2ea4ef27557
Hristo Bakardzhiev <h.bakardzhiev@gmx.com>
2022-06-25 17:53:02 +0200

    read character by character

--- src/main/java/com/example/demo/UtilHelpers.java
+++ src/main/java/com/example/demo/UtilHelpers.java
@@ -9,7 +9,11 @@ public final class UtilHelpers {
 
     public static final String SRC_MAIN_RESOURCES = "src/main/resources/";
 
-    public static final Integer CHARACTERS_TO_CACHE = 500;
+    public static final Integer ZERO_IN_ASCII = 48;
+    public static final Integer NINE_IN_ASCII = 57;
+    // new line ASCII characters
+    public static final Integer CARRIAGE_RETURN = 13;
+    public static final Integer NEW_LINE = 10;
 
     private UtilHelpers() {}
 }
--- src/main/java/com/example/demo/services/impl/ReadFileServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/ReadFileServiceImpl.java
@@ -7,69 +7,47 @@ import com.example.demo.UtilHelpers;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class ReadFileServiceImpl implements ReadFileService {
 
     public HashMap<Character, List<Position>> readFile(String input) throws IOException {
         final var filePath = (UtilHelpers.SRC_MAIN_RESOURCES + input);
-        FileInputStream inputStream = new FileInputStream(filePath);
 
-        final var hashMap = new HashMap<Character, List<Position>>();
-        Scanner sc = null;
-        try {
-            sc = new Scanner(inputStream, StandardCharsets.UTF_8);
+        try (FileInputStream inputStream = new FileInputStream(filePath)) {
+            final var hashMap = new HashMap<Character, List<Position>>();
             long rowNumber = 0;
-            while (sc.hasNextLine()) {
-
-                final var stringLine = sc.nextLine();
-                for (int start = 0; start < stringLine.length(); start += UtilHelpers.CHARACTERS_TO_CACHE) {
-                    final var choppedString = stringLine.substring(start, Math.min(stringLine.length(), start + UtilHelpers.CHARACTERS_TO_CACHE));
-                    final var charArray = choppedString.toCharArray();
-                    final var digitsWithPosition = getDigitAndPosition(charArray, rowNumber);
-                    digitsWithPosition.forEach(element -> {
-                        final var currDigit = element.getKey();
-                        final var currPosition = element.getValue();
-
-                        var currPositionList = hashMap.get(currDigit);
-                        if (currPositionList == null) {
-                            currPositionList = new ArrayList<Position>(Collections.singleton(currPosition));
-                        } else  {
-                            currPositionList.add(currPosition);
-                        }
-                        hashMap.put(currDigit, currPositionList);
-                    });
+            long colNumber = 0;
+            int asciiCharacter;
+            while ((asciiCharacter = inputStream.read()) != -1) {
+                if (isEndOfLine(asciiCharacter)) {
+                    rowNumber++;
                 }
 
-            rowNumber++;
-            }
-            if (sc.ioException() != null) {
-                throw sc.ioException();
-            }
+                if (isASCIIDigit(asciiCharacter)) {
+                    final Character currDigit = (char) asciiCharacter;
+                    final var currPosition = new Position(rowNumber, colNumber);
+                    var currPositionList = hashMap.get(currDigit);
+                    if (currPositionList == null) {
+                        currPositionList = new ArrayList<Position>(Collections.singleton(currPosition));
+                    } else {
+                        currPositionList.add(currPosition);
+                    }
+                    hashMap.put(currDigit, currPositionList);
+                }
 
-            return hashMap;
-        } finally {
-            inputStream.close();
-            if (sc != null) {
-                sc.close();
+
+                colNumber++;
             }
+            return hashMap;
         }
     }
 
-    private Stream<AbstractMap.SimpleImmutableEntry<Character, Position>> getDigitAndPosition(char[] charArray, long finalRowNumber) {
-        return IntStream.range(0, charArray.length)
-                .parallel()
-                .filter(elementIndex -> {
-                    final var currentCharElement = (int) charArray[elementIndex];
-                    // In asci numbers (0-9) reside between 47 and 58
-                    return currentCharElement <= 57 && currentCharElement >= 48;
-                })
-                .mapToObj(element -> new AbstractMap.SimpleImmutableEntry<Character, Position>(
-                        charArray[element], new Position(finalRowNumber, element)
-                ));
+    private Boolean isASCIIDigit(Integer asciiCharacter) {
+        return asciiCharacter <= UtilHelpers.NINE_IN_ASCII && asciiCharacter >= UtilHelpers.ZERO_IN_ASCII;
     }
 
+    private Boolean isEndOfLine(Integer asciiCharacter) {
+        return Objects.equals(asciiCharacter, UtilHelpers.CARRIAGE_RETURN) || Objects.equals(asciiCharacter, UtilHelpers.NEW_LINE);
+    }
 }
