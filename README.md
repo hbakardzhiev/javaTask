3e3919914e558d30317c0234b466685938f49e9c
Hristo Bakardzhiev <h.bakardzhiev@gmx.com>
2022-06-26 23:49:12 +0200

    fix row numbering + performance section readme

--- README.md
+++ README.md
@@ -9,24 +9,76 @@ It can be of any size and even if the entire file is written on a single line it
 
 # Output
 The output below is generated when the user passes the input.txt file to the Controller.
-```javascript
+```yaml
 {
-    "3": [
-        {
-            "row": 0,
-            "column": 3
-        }
-    ],
-    "4": [
-        {
-            "row": 0,
-            "column": 0
-        },
-        {
-            "row": 0,
-            "column": 6
-        }
-    ]
+  "0": [
+    {
+      "row": 1,
+      "column": 3
+    }
+  ],
+  "1": [
+    {
+      "row": 0,
+      "column": 0
+    },
+    {
+      "row": 1,
+      "column": 0
+    },
+    {
+      "row": 2,
+      "column": 0
+    }
+  ],
+  "2": [
+    {
+      "row": 0,
+      "column": 1
+    },
+    {
+      "row": 1,
+      "column": 1
+    },
+    {
+      "row": 2,
+      "column": 1
+    }
+  ],
+  "3": [
+    {
+      "row": 0,
+      "column": 2
+    },
+    {
+      "row": 1,
+      "column": 2
+    },
+    {
+      "row": 2,
+      "column": 2
+    }
+  ],
+  "8": [
+    {
+      "row": 3,
+      "column": 0
+    }
+  ],
+  "9": [
+    {
+      "row": 2,
+      "column": 3
+    }
+  ]
 }
 ```
-The ideaology of the output is that each number 
+The ideology of the output is that each number present in the user input would be on the 1st level and its occurrences would be on the 2nd level.
+This way you would only see numbers that are part of the file and all their exact places.
+
+# Performance
+The main function `readFile` does the parsing of the txt files using the following:
+    - uses hashMap to store the found digits and their position.
+    - tracks end of rows using `isEndOfLine`. 
+    - `isASCIIDigit` checks whether the current character is digit
+
--- src/main/java/com/example/demo/services/impl/ReadFileServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/ReadFileServiceImpl.java
@@ -5,47 +5,52 @@ import com.example.demo.modules.Position;
 import com.example.demo.services.interfaces.ReadFileService;
 import com.example.demo.UtilHelpers;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
-
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 
 public class ReadFileServiceImpl implements ReadFileService {
 
     @Async
-    public CompletableFuture<HashMap<Character, List<Position>>> readFile(String input) throws IOException {
+    public CompletableFuture<HashMap<Character, List<Position>>> readFile(String input) throws FileNotFoundException {
         final var filePath = (UtilHelpers.SRC_MAIN_RESOURCES + input);
-
-        try (FileInputStream inputStream = new FileInputStream(filePath)) {
+        try (final var inputStream = new FileReader(filePath)) {
             final var hashMap = new HashMap<Character, List<Position>>();
             long rowNumber = 0;
             long colNumber = 0;
             int asciiCharacter;
             while ((asciiCharacter = inputStream.read()) != -1) {
-                if (isEndOfLine(asciiCharacter)) {
-                    rowNumber++;
-                }
-
                 if (isASCIIDigit(asciiCharacter)) {
                     final Character currDigit = (char) asciiCharacter;
                     final var currPosition = new Position(rowNumber, colNumber);
-                    var currPositionList = hashMap.get(currDigit);
-                    if (currPositionList == null) {
-                        currPositionList = new ArrayList<Position>(Collections.singleton(currPosition));
-                    } else {
-                        currPositionList.add(currPosition);
-                    }
+                    final var currPositionList = generateNewPositionList(hashMap, currDigit, currPosition);
                     hashMap.put(currDigit, currPositionList);
                 }
-
-
                 colNumber++;
+                if (isEndOfLine(asciiCharacter)) {
+                    rowNumber++;
+                    colNumber = 0;
+                }
             }
             return CompletableFuture.completedFuture(hashMap);
         }
+        catch (FileNotFoundException exception) {
+            throw new FileNotFoundException("The current file does not exist.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<Position> generateNewPositionList(HashMap<Character, List<Position>> hashMap, Character currDigit, Position currPosition) {
+        var currPositionList = hashMap.get(currDigit);
+        if (currPositionList == null) {
+            currPositionList = new ArrayList<>(Collections.singleton(currPosition));
+        } else {
+            currPositionList.add(currPosition);
+        }
+        return currPositionList;
     }
 
     private Boolean isASCIIDigit(Integer asciiCharacter) {
